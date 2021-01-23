8fba87c5acc08805e5206ac4026c6ae3b1524cb5
BBlack-Hun <55784870+BBlack-Hun@users.noreply.github.com>
2021-01-23 21:59:51 +0900

    Create example3.py

---
+++ example3.py
@@ -0,0 +1,17 @@
+import csv
+
+f = open('Pycharm/seoul.csv')
+data = csv.reader(f)
+header = next(data)
+for row in data:
+    # 데이터의 마지막란(최고기온)이 공란이라면?
+    if row[-1] == '':
+        # -999라는 숫자를 넣는다.
+        row[-1] = -999
+    row[-1] = float(row[-1])
+    print(row)
+f.close()
+
+'''
+    이 코드에서는 오류가 발생하지 않는다.
+'''
