013ea852a0c4aa2acd22fcb612ceb76c5c7cda2e
BBlack-Hun <55784870+BBlack-Hun@users.noreply.github.com>
2021-01-24 16:13:54 +0900

    Update example2.py

--- example2.py
+++ example2.py
@@ -5,6 +5,7 @@ data = csv.reader(f)
 header = next(data)
 for row in data:
     # 누락된 데이터가 있으므로, 오류가 발생한다.
+    # 최고 기온이 적혀 있는 테이블로, str -> float형태로 바꾼다.
     row[-1] = float(row[-1])
     print(row)
 f.close()
